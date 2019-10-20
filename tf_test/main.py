c91685482d1bd3c7993de00f740962f3dd880cb0
sxy <you@example.com>
2019-10-21 00:10:23 +0800

    准备些 tornado 项目

--- tf_test/main.py
+++ tf_test/main.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 matrix1 = tf.constant([[3., 3.]])
 
 # 创建另外一个常量 op, 产生一个 2x1 矩阵.
-matrix2 = tf.constant([[2.],[2.]])
+matrix2 = tf.constant([[2.], [2.]])
 
 # 创建一个矩阵乘法 matmul op , 把 'matrix1' 和 'matrix2' 作为输入.
 # 返回值 'product' 代表矩阵乘法的结果.
